f9000c7e9f2cc6e92eb9e02e3e3fc92dd9d71d73
kiverak <kiverak@gmail.com>
2024-12-13 15:33:02 +0500

    first commit

---
+++ src/main/java/uz/kiverak/oauth2/spring/testoauth2/testoauth2/security/SpringSecurityConfig.java
@@ -0,0 +1,34 @@
+package uz.kiverak.oauth2.spring.testoauth2.testoauth2.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import uz.kiverak.oauth2.spring.testoauth2.testoauth2.converter.KCRoleConverter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KCRoleConverter());
+
+        http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("test/login").anonymous()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(customizer -> customizer
+                        .jwt(jwtCustomizer -> jwtCustomizer.jwtAuthenticationConverter(jwtAuthenticationConverter))
+                );
+
+        return http.build();
+    }
+
+}
---
+++ src/test/java/uz/kiverak/oauth2/spring/testoauth2/testoauth2/TestOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.kiverak.oauth2.spring.testoauth2.testoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
